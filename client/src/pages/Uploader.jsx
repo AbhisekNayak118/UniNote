6671d0f4767348a8ba1eab302ebd11c64d5a95b2
abhi-676 <nayakabhisek024@gmail.com>
2024-07-10 18:27:46 +0530

    Client added

---
+++ client/src/pages/Uploader.jsx
@@ -0,0 +1,11 @@
+// import React from "react"
+import NotesUpload from "../components/NotesUpload";
+const Uploader = () => {
+  return (
+    <div>
+      <NotesUpload />
+    </div>
+  )
+};
+
+export default Uploader;
